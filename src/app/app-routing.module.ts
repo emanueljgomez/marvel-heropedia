6c03e2ea4ac96efa27cd14fcd94f02277b1bf6e9
Emanuel J. Gomez Sanchez <emanueljgomez@gmail.com>
2023-01-13 12:32:12 -0300

    Base project setup with test components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
+import { Test2Component } from './components/test2/test2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'hero-list',
+    component: TestComponent
+  },
+
+  {
+    path: 'hero-detail',
+    component: Test2Component
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
