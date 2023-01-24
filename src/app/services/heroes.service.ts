0d85a57c028e4b9d0171f4c258d6e1efbf23243f
Emanuel J. Gomez Sanchez <emanueljgomez@gmail.com>
2023-01-24 12:31:27 -0300

    Testing 'offset' and 'limit' parameters on URL for HTTP calls

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -15,7 +15,8 @@ export class HeroesService {
   // through a MD5 Hash Generator ( https://letmegooglethat.com/?q=MD5+hash+generator )
   // The Public and Private Keys are provided by the API ( https://developer.marvel.com/ )
   HASH = '1c820bbadd4cd42eb3139d95fe35aa64';
-  URL_API = `https:gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
+  URL_API = `https:gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}&offset=0&limit=100`; // '&limit=100' controls amount of elements returned
+  URL_API_2 = `https:gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}&offset=100&limit=100`;
 
   // HTTP constructor:
   constructor( private http: HttpClient ) { }
@@ -23,6 +24,11 @@ export class HeroesService {
   // HTTP methods with Observables:
   public getAllHeroes(): Observable<any> {
     return this.http.get<any>(this.URL_API)
+      .pipe(map((data: any) => data.data.results))
+  }
+
+  public getAllHeroes200(): Observable<any> {
+    return this.http.get<any>(this.URL_API_2)
       .pipe(map((data: any) => data.data.results))
   }
 
